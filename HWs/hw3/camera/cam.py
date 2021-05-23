6c1aefbd70ce02736efd7e536cea8699ace13196
Javed Roshan <jroshan.tx.us@gmail.com>
2021-05-23 00:57:50 -0500

    hw3 changes

--- HWs/hw3/camera/cam.py
+++ HWs/hw3/camera/cam.py
@@ -2,7 +2,24 @@
 # this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
 import cv2
+import paho.mqtt.client as mqtt
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+LOCAL_MQTT_HOST="mosquitto-service"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="my_topic"
+
+local_mqttclient = mqtt.Client()
+local_mqttclient.on_connect = on_connect_local
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+
+def on_connect_local(client, userdata, flags, rc):
+        print("connected to local broker with rc: " + str(rc))
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # the index depends on your camera setup and which one is your USB camera.
 # you may need to change to 1 depending on your local config
@@ -10,13 +27,30 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, img = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    # cv2.imshow('frame', gray)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        gray_img = gray[y:y+h, x:x+w]
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        _,msg_to_Send = cv2.imencode('.png', gray_img)
+        bin_msg = png.tobytes()
+        #publish the message
+        local_mqttclient.publish(LOCAL_MQTT_TOPIC, bin_msg)
+
+    # cv2.imshow('img', img)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- HWs/hw3/listener/listener.py
+++ HWs/hw3/listener/listener.py
@@ -3,7 +3,8 @@ import paho.mqtt.client as mqtt
 
 LOCAL_MQTT_HOST="localhost"
 LOCAL_MQTT_PORT=1883
-LOCAL_MQTT_TOPIC="w251_hw3"
+LOCAL_MQTT_TOPIC="my_topic"
+#LOCAL_MQTT_TOPIC="w251_hw3"
 
 def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
