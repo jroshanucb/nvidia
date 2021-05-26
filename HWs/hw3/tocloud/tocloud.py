56a586019073f8e122b8948dcfe4c1d12a863173
Javed Roshan <jroshan.tx.us@gmail.com>
2021-05-25 23:07:18 -0500

    syntax error fox in tocloud.py

--- HWs/hw3/tocloud/tocloud.py
+++ HWs/hw3/tocloud/tocloud.py
@@ -18,7 +18,7 @@ def on_connect_remote(client, userdata, flags, rc):
 
 def on_message(client,userdata, msg):
   try:
-    print("message received: ",msg.payload))
+    print("message received: ",msg.payload)
     # if we wanted to re-publish this message, something like this should work
     msg = msg.payload
     remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
