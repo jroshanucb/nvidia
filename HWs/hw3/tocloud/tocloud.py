928bbd4d07a1c34e70705f221f7ba9d9b184c9ce
Javed Roshan <jroshan.tx.us@gmail.com>
2021-05-25 23:02:22 -0500

    cloud message pusher yaml

--- HWs/hw3/tocloud/tocloud.py
+++ HWs/hw3/tocloud/tocloud.py
@@ -5,8 +5,8 @@ LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="my_topic"
 #LOCAL_MQTT_TOPIC="w251_hw3"
 
-REMOTE_MQTT_HOST="<get IP>"
-REMOTE_MQTT_PORT=<get Port>
+REMOTE_MQTT_HOST="34.223.41.215"
+REMOTE_MQTT_PORT=30789
 REMOTE_MQTT_TOPIC="my_topic"
 
 def on_connect_local(client, userdata, flags, rc):
@@ -18,10 +18,11 @@ def on_connect_remote(client, userdata, flags, rc):
 
 def on_message(client,userdata, msg):
   try:
-    print("message received: ",str(msg.payload.decode("utf-8")))
+    print("message received: ",msg.payload))
     # if we wanted to re-publish this message, something like this should work
     msg = msg.payload
     remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+    print("mesg published to cloud queue")
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
