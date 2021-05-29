3e6123cdcf2e4df0d85091014b672b0f98372c2a
Javed Roshan <jroshan.tx.us@gmail.com>
2021-05-28 23:33:52 -0500

    cam.py updated

--- HWs/hw3/camera/cam.py
+++ HWs/hw3/camera/cam.py
@@ -11,10 +11,14 @@ LOCAL_MQTT_TOPIC="my_topic"
 def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
 
+def on_publish_local(client, userdata, result):
+    print("data published on the queue with result: ", result)
+
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml')
 
 local_mqttclient = mqtt.Client()
 local_mqttclient.on_connect = on_connect_local
+local_mqttclient.on_publish = on_publish_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 
 # the index depends on your camera setup and which one is your USB camera.
